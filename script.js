e6f51f6a48016027116b6c3f25dd8ea467739f99
Ashwini Gosavi <120784052+ashwinigosavi27@users.noreply.github.com>
2023-05-24 15:37:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-let yourArray = [["apple", 10, true], ["banana", 5, false], [42, "orange", true]];
+let yourArray = [["apple", 10, true], ["banana", 5, false], [42, "orange", true], ["kiwi", 30, true], ["chiku", 14, false]];
 
 
